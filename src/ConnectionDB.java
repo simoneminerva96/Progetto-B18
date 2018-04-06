6d2f9be4aa2c1bb7efb785b59a44d8978d0956e7
NichettiM <mattia.nichetti01@universitadipavia.it>
2018-04-06 21:14:45 +0200

    Modifica della connessione al db, con collegamento alle procedure mysql

--- src/ConnectionDB.java
+++ src/ConnectionDB.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.io.IOException;
 
 public class ConnectionDB {
-    public static void main(String[] args) throws SQLException {
+    /*public static void main(String[] args) throws SQLException {
         Connection cn; //connessionea al db
         Statement st; //permette di costruire le query
         ResultSet rs; //permette di ottenere il risultato della query
@@ -11,12 +11,15 @@ public class ConnectionDB {
 
 
 
-            cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trivial?user=root&password=root");
+        cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trivial?user=root&password=root");
             //trivial è il nome del database
+
+
 
 
         sql = "SELECT * FROM domande;";
         // ________________________________query
+
         try {
             st = cn.createStatement(); //creo un statement sulla connessione
             rs = st.executeQuery(sql); //faccio la query sullo statement
@@ -25,6 +28,43 @@ public class ConnectionDB {
         } catch (SQLException e) {
             System.out.println("errore:" + e.getMessage());
         } // fine try-catch
+
+
         cn.close(); // chiusura connessione
-    }// fine main
+
+
+    }// fine main*/
+
+
+    public static void main(String[] args) {
+        getPlayer("Mirko", "12345");
+    }
+
+    /**
+     * @param IDNAME
+     * @param PW
+     */
+    public static void getPlayer(String IDNAME, String PW) {
+        //
+        String query = "{ call ADD_PLAYER(?,?) }";
+        ResultSet rs;
+
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trivial?user=root&password=root");
+             CallableStatement stmt = conn.prepareCall(query)) {
+
+            stmt.setString(1, IDNAME);
+            stmt.setString(2, PW);
+
+            rs = stmt.executeQuery();
+
+            /*while (rs.next()) {
+                System.out.println(String.format("%s - %s", rs.getString("ID") + " " + rs.getString("PASSWORD")));
+            }*/
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
 }
