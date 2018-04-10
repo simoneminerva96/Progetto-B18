2a36a5c1baf3b686c4f88cef834e6b3f5c80ef06
Rita96 <32876112+Rita96@users.noreply.github.com>
2018-04-10 14:31:42 +0200

    Add files via upload
    Primo abbozzo di gioco fatto con Slick2D

---
+++ Trivial Pursuit/src/javagame/Game.java
@@ -0,0 +1,37 @@
+package javagame;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Game extends StateBasedGame{
+    public static final String gameName = "Trivial Pursuit";
+    public static final int menu = 0;
+    public static final int play = 1;
+
+    public Game(String gameName) {
+        super(gameName);
+        this.addState(new Menu(menu));
+        this.addState(new Play(play));
+    }
+
+    /*
+    Ogni State è una "schermata" di gioco. Qui le inizializzo
+     */
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.getState(menu).init(gc,this); // Inizializzo Menu iniziale del gioco
+        this.getState(play).init(gc, this); // Inizializzo lo screen del gioco
+        this.enterState(menu); // qual è il primo screen che devo mostrare allo user?
+    }
+
+    public static void main(String[] args) {
+        AppGameContainer appgc; // the window for the game
+        try {
+            //a windows that contains a game named gameName
+            appgc = new AppGameContainer(new Game(gameName));
+            appgc.setDisplayMode(400,400, false);
+            appgc.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+}
