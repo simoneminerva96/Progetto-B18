a9abe1583423a42f2ceb51fd39eb64998e10df9b
NichettiM <mattia.nichetti01@universitadipavia.it>
2018-04-26 15:12:52 +0200

    Prova per Client/Server non ancora funzionante

--- src/ClientServer/Server.java
+++ src/ClientServer/Server.java
@@ -1,4 +1,17 @@
 package ClientServer;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
+
+    //il compito del server è quella di accettare degli input dai client e processi e ridare indietro qualcosa
+    public static void main(String[] args) throws IOException {
+
+        //il server delle avere la stessa porta usata nel client
+        ServerSocket s1 = new ServerSocket(1342);
+
+
+
+    }
 }
