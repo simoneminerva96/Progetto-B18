2b1fbf05915e002f65f6ce25b2c0b58e4463708a
NichettiM <mattia.nichetti01@universitadipavia.it>
2018-04-26 15:36:26 +0200

    Sistemata classe Server e il programma di prova è funzionante

--- src/ClientServer/Server.java
+++ src/ClientServer/Server.java
@@ -1,7 +1,10 @@
 package ClientServer;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
@@ -11,7 +14,19 @@ public class Server {
         //il server delle avere la stessa porta usata nel client
         ServerSocket s1 = new ServerSocket(1342);
 
+        //usata per accettare la richiesta ad s1 da parte el client
+        Socket ss =  s1.accept();
 
+        //prende il numero dalla richiesta del client
+        Scanner sc = new Scanner(ss.getInputStream());
+        int number = sc.nextInt();
+
+
+        int temp = number*2;
+
+        //bisogna ora ripassare il risultato al client
+        PrintStream p = new PrintStream(ss.getOutputStream());
+        p.println(temp);
 
     }
 }
