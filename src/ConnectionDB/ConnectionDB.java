84117ced636f9dc2b956a0438077376a664794e5
Mattia Nichetti <mattianichetti@gmail.com>
2018-05-17 15:35:35 +0200

    Connessione DB SSL

--- src/ConnectionDB/ConnectionDB.java
+++ src/ConnectionDB/ConnectionDB.java
@@ -29,7 +29,7 @@ public class ConnectionDB {
      */
     public ArrayList<Question> getQuestion (String cod) throws SQLException{
 
-        cn = DriverManager.getConnection("jdbc:mysql://10.87.144.91:3306/trivial?user=root&password=root");
+        cn = DriverManager.getConnection("jdbc:mysql://10.87.144.91:3306/trivial?useSSL=false", "root", "root");
         sql = "select ID_QUEST, DESCRIZIONE, RISPOSTA, VALUE from domande join risposte on ID_QUEST = ID_DOMANDA where ID_QUEST LIKE \"" + cod + "%\"";
         // ________________________________query
         ArrayList<Question> questions=new ArrayList<Question>();
@@ -137,7 +137,7 @@ public class ConnectionDB {
         String query = "{ ?=call ADD_PLAYER1(?,?) }";
         ResultSet rs;
         String returnMessage="";
-        try (Connection conn = DriverManager.getConnection("jdbc:mysql://10.87.144.91:3306/trivial?user=root&password=root");
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://10.87.144.91:3306/trivial?useSSL=false", "root", "root");
              CallableStatement stmt = conn.prepareCall(query)) {
 
             stmt.registerOutParameter(1, Types.VARCHAR);
