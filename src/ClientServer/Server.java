1e8664b0d9ef823277c86abbdbb5caad3f7d43e8
Rita96 <rita.dicecca01@universitadipavia.it>
2018-06-12 17:28:50 +0200

    modifiche

--- src/ClientServer/Server.java
+++ src/ClientServer/Server.java
@@ -3,15 +3,13 @@ package ClientServer;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 
-public class Server implements Runnable {
+public class Server implements Runnable {  // ClientManager
 
     private DataInputStream in; //per ricevere da client
     private DataOutputStream out; //per inviare al client
@@ -23,7 +21,7 @@ public class Server implements Runnable {
     }
 
     //il compito del server è quella di accettare degli input dai client e processi e ridare indietro qualcosa
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {  // Spostarlo in una classe a parte (ServerApplication)
 
         ServerSocket server ;
         Socket socketClient ;
@@ -52,10 +50,6 @@ public class Server implements Runnable {
 
         try {
             in = new DataInputStream(socketClient.getInputStream()); //input da client
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
             out = new DataOutputStream(socketClient.getOutputStream()); //output a client
         } catch (IOException e) {
             e.printStackTrace();
