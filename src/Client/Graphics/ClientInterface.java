f5fece20e2da99a8549649b81288e59228c3ee0e
Rita96 <rita.dicecca01@universitadipavia.it>
2018-06-18 17:42:43 +0200

    invio e ricevo con la serializzazione

--- src/Client/Graphics/ClientInterface.java
+++ src/Client/Graphics/ClientInterface.java
@@ -8,6 +8,7 @@ import java.net.UnknownHostException;
 
 public class ClientInterface implements Serializable {
     private ObjectOutputStream out1 = null;
+    private ObjectInputStream in = null;
 
     public ClientInterface(){ }
 
@@ -16,12 +17,18 @@ public class ClientInterface implements Serializable {
             Socket server = new Socket(InetAddress.getLocalHost(),8888);
             System.out.println("Connessione al server effettuata");
             out1 = new ObjectOutputStream(server.getOutputStream());
+            in = new ObjectInputStream(server.getInputStream());
             DieGUI prova = new DieGUI();
             System.out.println("Object to be written: "+ prova);
             out1.writeObject(prova);
+
+            DieGUI die = (DieGUI) in.readObject();
+            System.out.println("Object received: " +die);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
--- src/Server/GameClasses/ServerInterface.java
+++ src/Server/GameClasses/ServerInterface.java
@@ -8,6 +8,7 @@ import java.net.Socket;
 public class ServerInterface extends Thread implements Serializable {
     private Socket socketClient ;
     private ObjectInputStream in1 = null;
+    private ObjectOutputStream out = null;
 
     public ServerInterface(Socket socketClient){
         this.socketClient = socketClient;
@@ -17,9 +18,11 @@ public class ServerInterface extends Thread implements Serializable {
     public void run() {
         try {
             in1 = new ObjectInputStream(socketClient.getInputStream());
+            out = new ObjectOutputStream(socketClient.getOutputStream());
             System.out.println("aspetto un messaggio...");
             DieGUI die = (DieGUI) in1.readObject();
             System.out.println("Object received: " +die);
+            out.writeObject(die);
             socketClient.close(); //termino la connesione al client
         } catch (IOException e) {
             e.printStackTrace();
