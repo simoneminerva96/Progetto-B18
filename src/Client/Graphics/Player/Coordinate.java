1cfe852b1b6d7cfde56e7f241ffd4487abf91af3
thenuste <stefanokothuwa@live.it>
2018-07-03 12:11:51 +0200

    Rimozione classe CoordinateSystem

--- src/Client/Graphics/Player/Coordinate.java
+++ src/Client/Graphics/Player/Coordinate.java
@@ -23,6 +23,81 @@ public class Coordinate {
         this.y = y;
     }
 
+    public void calculate(Coordinate minime,Coordinate massime,int die,Direction direction){
+        int minx=minime.getX();
+        int miny=minime.getY();
+        int maxx=massime.getX();
+        int maxy=massime.getY();
+
+        int mul=1,confront,temp;
+        if(y==miny){
+            if(direction==Direction.FORWARD){
+                mul=-1;
+                confront=maxx*mul;
+                temp=(x+2*die)*mul;
+            }else{
+                confront=minx*mul;
+                temp=(x-2*die)*mul;
+            }
+            if(temp<confront){
+                temp=confront-temp;
+                y+=temp;
+                x=confront*mul;
+            }else{
+                x=temp*mul;
+            }
+        }else if(x==maxx){
+            if(direction==Direction.FORWARD){
+                mul=-1;
+                confront=maxy*mul;
+                temp=(y+2*die)*mul;
+            }else{
+                confront=miny*mul;
+                temp=(y-2*die)*mul;
+            }
+            if(temp<confront){
+                temp=confront-temp;
+                x-=temp;
+                y=confront*mul;
+            }else{
+                y=temp*mul;
+            }
+        }else if(y==maxy){
+            if(direction==Direction.FORWARD){
+                mul=-1;
+                confront=minx*mul;
+                temp=(x-2*die)*mul;
+            }else{
+                confront=miny*mul;
+                temp=(x+2*die)*mul;
+            }
+            if(temp>confront){
+                temp=temp-confront;
+                x=confront*mul;
+                y-=temp;
+            }else{
+                x=temp*mul;
+            }
+        }else if(x==minx){
+            if(direction==Direction.FORWARD){
+                mul=-1;
+                confront=miny*mul;
+                temp=(y-2*die)*mul;
+            }else{
+                confront=maxy*mul;
+                temp=(y+2*die)*mul;
+            }
+            if(temp>confront){
+                temp=temp-confront;
+                y=confront*mul;
+                x+=temp;
+            }else{
+                y=temp*mul;
+            }
+
+        }
+    }
+
 
 
 }
