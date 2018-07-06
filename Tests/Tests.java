26ee235925f9989f59f4ef92a0af94d18570d6ff
Mattia Nichetti <mattianichetti@gmail.com>
2018-07-07 01:51:30 +0200

    aggiunto testDB

--- Tests/Tests.java
+++
@@ -1,8 +0,0 @@
-package Server.GameClasses.Tests;
-
-/**
- * test di alcuni metodi della logica per verificarne il comportamento con casi limite e non
- */
-public class Tests {
-
-}
---
+++ Tests/Tests/TestsDB.java
@@ -0,0 +1,41 @@
+package Tests;
+
+import Client.Graphics.Player.Coordinate;
+import Client.Graphics.Player.Player;
+import Server.GameClasses.ConnectionDB.ConnectionDB;
+import Server.GameClasses.Credenziali;
+
+import java.util.Scanner;
+
+
+public class TestsDB {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static void TestDB(){
+        Object test = null;
+        String Nome;
+        String Pass;
+        ConnectionDB connection = new ConnectionDB();
+        Credenziali cred;
+
+        System.out.println("Dammi un nome da provare ");
+        Nome = scanner.nextLine();
+
+        System.out.println("Dammi la password");
+        Pass = scanner.nextLine();
+
+        cred = new Credenziali(Nome, Pass);
+
+        test = connection.ExistsPlayer(cred);
+        System.out.println(test);
+
+        assert test != null : "Login andato male";
+    }
+
+    public static void main(String[] args) {
+        TestsDB test = new TestsDB();
+        TestsDB.TestDB();
+    }
+
+}
